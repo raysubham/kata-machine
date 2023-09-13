0bc9f0490a64d3841366562989d9d73272a22fc1
Subham Ray <self.programmed@gmail.com>
2023-09-13 22:37:25 +0530

    some solutions

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    while (low < high) {
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+        if (value === needle) return true;
+        else if (value > needle) {
+            high = mid;
+        } else if (value < needle) {
+            low = mid + 1;
+        }
+    }
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    for (i = i + jumpAmount; i < breaks.length; ) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i = i - jumpAmount;
+
+    for (let j = 0; j < jumpAmount; i++, j++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
